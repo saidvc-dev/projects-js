4e826772e64ba42cf55fe3beea935abac714644b
Said Valencia Castrillo <50384031+said05@users.noreply.github.com>
2022-05-10 15:43:21 -0500

    keyboard component

--- componente de teclado/main.js
+++ componente de teclado/main.js
@@ -60,15 +60,80 @@ const keys = [
 ];
 
 let mayus = false;
-let shit = false;
+let shift = false;
+let current = null;
+renderKeyboard();
 
 function renderKeyboard() {
-  const keyboardContainer = document.querySelector('#keyboardContainer');
+  const keyboardContainer = document.querySelector('#keyboard-container');
   let empty = `<div class="key-empty"></div>`;
+  const layers = keys.map((layer, i) => {
+    return layer.map((key) => {
+      if (key[0] === 'SHIFT') {
+        return `<button class="key key-shift  ${shift ? 'activated' : ''}">${
+          key[0]
+        }</button>`;
+      }
+      if (key[0] === 'MAYUS') {
+        return `<button class="key key-mayus ${mayus ? 'activated' : ''}">${
+          key[0]
+        }</button>`;
+      }
+      if (key[0] === 'SPACE') {
+        return `<button class="key key-space"></button>`;
+      }
 
-  const layers = keys.map((layer) => {
-      return layer.map((key) => {
-        
+      //printing key[0] or key[1]
+      return `<button class="key key-normal">${
+        shift
+          ? key[1]
+          : mayus &&
+            key[0].toLowerCase().charCodeAt(0) >= 97 &&
+            key[0].toLowerCase().charCodeAt(0) <= 122
+          ? key[1]
+          : key[0]
+      }</button>`;
+    });
+  });
+
+  layers[0].push(empty);
+  layers[1].unshift(empty);
+
+  const htmlLayers = layers.map((layer) => {
+    return layer.join('');
+  });
+
+  keyboardContainer.innerHTML = '';
+
+  htmlLayers.forEach((layer) => {
+    keyboardContainer.innerHTML += `<div class="layer">${layer}</div>`;
+  });
+
+  document.querySelectorAll('.key').forEach((key) => {
+    key.addEventListener('click', (e) => {
+      if (current) {
+        if (key.textContent === 'SHIFT') {
+          shift = !shift;
+        } else if (key.textContent === 'MAYUS') {
+          mayus = !mayus;
+        } else if (key.textContent === '') {
+          current.value += ' ';
+        } else {
+          current.value += key.textContent.trim();
+          if (shift) {
+            shift = false;
+          }
+        }
+        renderKeyboard();
+        current.focus();
+      }
     });
   });
 }
+
+document.querySelectorAll('input').forEach((input) => {
+  input.addEventListener('focusin', (e) => {
+    current = e.target;
+    console.log(current);
+  });
+});
